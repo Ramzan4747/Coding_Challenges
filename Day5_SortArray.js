8d4969905887979795ab28b19b4909891ac2d4b8
unknown <ramzan10.jhang@gmail.com>
2024-06-11 09:59:55 +0500

    Problem_5 Sorting numeric array

---
+++ Day5_SortArray.js
@@ -0,0 +1,15 @@
+// Sorting Array 
+const sortArray = (arr) =>{
+    // using comparison is sort method for assending
+return arr.sort((a,b) => a-b)
+// using comparison is sort method for Descending
+// return arr.sort((a,b) => b-a)
+
+}
+
+console.log(sortArray([2,20,15,17,13]));
+
+
+
+
+
